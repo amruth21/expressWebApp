c7811a30ada32e4ecc63e08247a96e5674cb143e
Amruth Nare <45131882+amruth21@users.noreply.github.com>
2019-06-17 17:53:51 -0400

    extra file paths

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/src/pages/index.html'))
 })
 
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, '/src/pages/about.html'))
+})
+
 app.listen(port, function () {
     console.log('Server running on http://localhost:' + port);
 })
